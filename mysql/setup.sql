3bf37447e9d0005f2c7b41a80d6a05200da9cbf2
bmeistergeneral <brecon.morgan@gmail.com>
2024-11-20 01:22:49 -0500

    Added sample data

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -1,15 +1,12 @@
--- DROP DATABASE IF EXISTS student_passwords;
-
+DROP DATABASE IF EXISTS student_passwords;
 CREATE DATABASE student_passwords;
-
--- DROP USER IF EXISTS 'passwords_user'@'localhost';
-
+DROP USER IF EXISTS 'passwords_user'@'localhost';
 CREATE USER 'passwords_user'@'localhost';
 GRANT ALL ON student_passwords.* TO 'passwords_user'@'localhost';
 
-SET block_encryption_mode = 'aes-256-cbc';
-SET @key_str = UNHEX(SHA2('ThisIsARegularStatement', 512));
-SET @init_vector = 1010101010101010;
+-- SET block_encryption_mode = 'aes-256-cbc';
+-- SET @key_str = UNHEX(SHA2('ThisIsARegularStatement', 512));
+-- SET @init_vector = 1010101010101010;
 
 USE student_passwords;
 
@@ -33,10 +30,29 @@ CREATE TABLE Passwords (
 password_id MEDIUMINT NOT NULL AUTO_INCREMENT,
 user_id MEDIUMINT NOT NULL,
 website_id MEDIUMINT NOT NULL,
-enc_password varbinary(512) NOT NULL,
+enc_password varchar(255) NOT NULL,
 comment varchar(255),
 create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (password_id),
 FOREIGN KEY (website_id) REFERENCES Websites(website_id)
 FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
+
+insert into Users (first_name, last_name, username, email)
+values
+("Brecon", "Morgan", "bmeisteriscool", "breconiusmorg@gmail.com"),
+('Dylan', 'Heinen', 'dheinen88', 'heinen@hotmail.com'),
+('Bob', 'Johnson', 'bjohnsonFTW', 'bob@hotmail.com');
+
+insert into Websites(website_name, website_url) values
+('Facebook', 'https://www.facebook.com'),
+('Instagram', 'https://www.instagram.com'),
+('Google', 'https://www.google.com')
+
+insert into Passwords (user_id, website_id, enc_password, comment)
+values
+(1, 1, 'google123', 'My Google account'),
+(1, 2, 'facebook149', 'My Facebook login'),
+(2, 3, 'forthegram789', 'Code repository access'),
+(3, 1, 'googlepwd', 'Work email account'),
+(2, 2, 'fblogin756', 'Personal Facebook');
